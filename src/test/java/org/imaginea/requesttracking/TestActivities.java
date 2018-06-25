ced27bf51242d5268a6120c70390fc1848baa5ec
saikrishna1214 <saikrishnadec14@gmail.com>
2018-06-25 17:01:29 +0530

    added test cases for ActivityService

---
+++ src/test/java/org/imaginea/requesttracking/TestActivities.java
@@ -0,0 +1,20 @@
+package org.imaginea.requesttracking;
+
+import org.imaginea.requesttracking.service.impl.ActivityServiceImpl;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TestActivities extends CustomerServicePortalTests {
+
+	@Autowired
+	private ActivityServiceImpl activityService;
+	
+	@Test
+	public void testActivity() {
+		
+		Assert.assertEquals(activityService.getActivities(4).size(), 0);
+		
+	}
+
+}
